c08da50499fbd5d054a32318b5563c2ddc5549f8
Tafowo Cedric <tafowocedric@gmail.com>
2021-09-20 16:17:37 +0100

    runjs syntax error fix

--- run.js
+++ run.js
@@ -1,28 +1,31 @@
-const pm2 = require('pm2');
+const pm2 = require("pm2");
 
-pm2.connect(function(err) {
+pm2.connect(function (err) {
     if (err) {
         console.log(err);
         process.exit(2);
-    };
-    
-    pm2.start({
-        script: 'npm start',
-        autorestart: true,
-        name: 'queenyekelunrivaled'
-    }, (err, apps) => {
-        if (err) {
-            console.log(err);
-            return pm2.disconnect();
+    }
+
+    pm2.start(
+        {
+            script: "npm start",
+            autorestart: true,
+            name: "queenyekelunrivaled",
+        },
+        (err, apps) => {
+            if (err) {
+                console.log(err);
+                return pm2.disconnect();
+            }
+
+            pm2.list((err, list) => {
+                console.log(err, list);
+
+                pm2.restart("queenyekelsunrivaled", (err, proc) => {
+                    // disconnect from pm2
+                    pm2.disconnect();
+                });
+            });
         }
-        
-        pm2.list((err, list) => {
-            console.log(err, list);
-        
-            pm2.restart('queenyekelsunrivaled', (err, proc) => {
-                // disconnect from pm2
-                pm2.disconnect()
-            };
-        });
-    });
+    );
 });
